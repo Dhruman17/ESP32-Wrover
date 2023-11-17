84b2ebc36def418ecaa3899d9a03ee4bab6c898b
Dhruman17 <dhruman@skyacres.ca>
2023-11-17 16:20:26 -0500

    Final commit for Frequency manipulation and brightness management

--- Frequency Management Testing/src/main.cpp
+++ Frequency Management Testing/src/main.cpp
@@ -1,18 +1,71 @@
-#include <Arduino.h>
+#define BLYNK_TEMPLATE_ID "TMPL23f8-CzzJ"
+#define BLYNK_TEMPLATE_NAME "Quickstart Template"
+#define BLYNK_AUTH_TOKEN "RakzjAVWVXKlhJqbEzpj0daZgiYcq9I9"
+#include <WiFi.h>
+#include <BlynkSimpleEsp32.h>
 
-// put function declarations here:
-int myFunction(int, int);
+char ssid[] = "BELL805";
+char pass[] = "MceWan@444";
 
+int ledPins[] = {2, 5, 15};
+int numLeds = 3;
+
+int blinkFrequency = 1000; // Default frequency in milliseconds
+int brightness = 100;      // Default brightness (0-255)
+
+BlynkTimer timer;
+
+void blinkLed();
+
+BLYNK_WRITE(V31) {
+  // Get the slider value and update the blinkFrequency
+  blinkFrequency = param.asFloat();
+
+  // Convert Hertz to milliseconds
+  int interval = (int)(1000.0 / blinkFrequency);
+
+  // Update the interval of the timer based on the new blinkFrequency
+  timer.setInterval(interval, blinkLed);
+  
+  // Notify Blynk about the current frequency
+  Blynk.virtualWrite(V31, blinkFrequency);
+}
+
+BLYNK_WRITE(V32) {
+  // Get the brightness value from the Number Input widget (1-100)
+  brightness = param.asInt();
+
+  // Notify Blynk about the current brightness
+  Blynk.virtualWrite(V32, brightness);
+}
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);
+
+  for (int i = 0; i < numLeds; i++) {
+    pinMode(ledPins[i], OUTPUT);
+  }
+
+  // Initial conversion from Hertz to milliseconds
+  int interval = (int)(1000.0 / blinkFrequency);
+  timer.setInterval(interval, blinkLed);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  Blynk.run();
+  timer.run();
+  Blynk.syncVirtual(V31);
+    Blynk.syncVirtual(V32);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+
+void blinkLed() {
+  for (int i = 0; i < numLeds; i++) {
+    // Toggle the LED state
+    digitalWrite(ledPins[i], !digitalRead(ledPins[i]));
+
+    // Set the LED brightness
+    analogWrite(ledPins[i], brightness * 2.55); // Map 1-100 to 0-255
+  }
 }
