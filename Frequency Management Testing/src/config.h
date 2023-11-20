d327a7061b73dd227ac56d89444d934d158dc85d
Dhruman17 <dhruman@skyacres.ca>
2023-11-20 15:55:32 -0500

    Added Config file for dynamic approach

---
+++ Frequency Management Testing/src/config.h
@@ -0,0 +1,8 @@
+#define BLYNK_TEMPLATE_ID "TMPL23f8-CzzJ"
+#define BLYNK_TEMPLATE_NAME "Quickstart Template"
+#define BLYNK_AUTH_TOKEN "RakzjAVWVXKlhJqbEzpj0daZgiYcq9I9"
+char ssid[] = "BELL805";
+char pass[] = "MceWan@444";
+
+int ledPins[] = {2, 5, 15};
+int numLeds = 3;
