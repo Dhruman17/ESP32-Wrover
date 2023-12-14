32d325a7c202652e9863b7c37421f8f8aabc0451
Dhruman17 <dhruman@skyacres.ca>
2023-12-14 15:44:41 -0500

    Final commit for temperature sensors

--- Testing Skyboard/src/SkyBoardAlpha3.h
+++ Testing Skyboard/src/SkyBoardAlpha3.h
@@ -3,27 +3,39 @@
 // WiFi credentials
 #define WIFI_SSID "BELL805"
 #define WIFI_PASS "MceWan@444"
+
+#define numberOfAtomizers 8
 // Pins for LEDs
-const int LED_PINS[] = {15, 2, 5, 4, 14, 17, 16, 19};
+const int atomizerPins[] = {15, 2, 5, 4, 14, 17, 16, 19};
+// const int atomizerPins[] = {23,19,18,25,17,16,04,2};
 // Virtual pins for Blynk
-const int LED_VPINS[] = {V1, V2, V3, V6, V7, V8, V9, V10};
-const int ON_TIME_SLIDER_VPIN = V4;
-const int OFF_TIME_SLIDER_VPIN = V5;
-// LEDC PWM Configuration   
+const int vPinAtomizerOnTime = V4;
+const int vPinAtomizerOffTime = V5;
+const int vPinAtomizers[] = {V1, V2, V3, V6, V7, V8, V9, V10};
+bool atomizerState[8] = {false, false, false, false, false, false, false, false};
+// LEDC PWM Configuration
 #define PWM_FREQUENCY 108000
 #define PWM_RESOLUTION 4
 #define PWM_MAX_DUTY_CYCLE 15
 // Float Switches
-#define NUM_FLOAT_SWITCHES 10
-const int FLOAT_SENSOR_PINS[] = {23, 25, 26, 27, 32, 33, 34, 35, 36, 39};
-const int FLOAT_SENSOR_VPINS_START = V12;
-const int FLOAT_SENSOR_VPINS[] = {V12, V13, V14, V15, V16, V17, V18, V19, V20, V21};
-
-const int TEMP_SWITCH_VPIN = V22;
+#define numberOfFloatSensors 10
+const int floatSensorPins[] = {23,25,26,27,32,33,34,35,36,39};
+// const int floatSensorPins[] = {23,27,26,25,33,32,35,34,36,39};
+const int vPinFloatSwitchStartingIndex = V12;
+const int vPinFloatSwitches[] = {V12, V13, V14, V15, V16, V17, V18, V19, V20, V21};
 #endif // CONFIG_H
 //The above configuration are as per Dhruman's Board.
 // IMP!!! Please set number of atomizers, sensors, atomizer pins, sensor pins, and vPinAtomizers,VPinSwitches according to use case
+
+// For Board JST V3 onwards
 // 4,5,2,14,15,17,16,19 -> Atomizer pins
 // 23,25,26,27,32,33,34,35,36,39 -> Sensor Pins
 // A1=4, A2=5, A3=2, A4=14, A5=15, A6=17, A7=16, A8=19
 // S1=23, S2=25, S3=26, S4=27, S5=32, S6=33, S7=34, S8=35, S9=36, S10=39
+// const int atomizerPins[] = {4,5,2,14,15,17,16,19};
+// const int floatSensorPins[] = {23,25,26,27,32,33,34,35,36,39};
+
+
+// For Board JST V2.1
+// const int atomizerPins[] = {23,19,18,25,17,16,04,2};
+// const int floatSensorPins[] = {23,27,26,25,33,32,35,34,36,39};
--- Testing Skyboard/src/main.cpp
+++ Testing Skyboard/src/main.cpp
@@ -3,7 +3,9 @@
 #define BLYNK_AUTH_TOKEN "RakzjAVWVXKlhJqbEzpj0daZgiYcq9I9"
 #include <Arduino.h>
 #include <BlynkSimpleEsp32.h>
+#include <driver/ledc.h>
 #include "SkyBoardAlpha3.h"
+
 #include <DHT.h>
 
 #define DHTPIN 21
@@ -11,64 +13,73 @@
 DHT dht(DHTPIN, DHTTYPE); // Use IO21 (SDA) for the temperature sensor
 
 #define TEMP_SWITCH_VPIN V22
-bool ledState[8] = {false, false, false, false, false, false, false, false};
 int atomizerOnTime = 0;
 int atomizerOffTime = 0;
 unsigned long prevTime = 0;
 bool isOnTime = true;
-int floatSensorStates[NUM_FLOAT_SWITCHES] = {0};
+int floatSensorStates[numberOfFloatSensors] = {0};
 unsigned long lastTemperatureSendTime = 0;
-
 void setup() {
   Serial.begin(9600);
   Blynk.begin(BLYNK_AUTH_TOKEN, WIFI_SSID, WIFI_PASS);
-
-  pinMode(TEMP_SWITCH_VPIN, INPUT);
-
-  for (int i = 0; i < NUM_FLOAT_SWITCHES; i++) {
-    pinMode(FLOAT_SENSOR_PINS[i], (i < 6) ? INPUT_PULLUP : INPUT);
+  // delay(1500);
+  // This delay gives the chance to wait for a Serial Monitor without blocking if none is found
+  //You can also specify server:
+  //Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass, "blynk.cloud", 80);
+  //Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass, IPAddress(192,168,1,100), 8080);
+  for (int i = 0; i < numberOfFloatSensors; i++) {
+    pinMode(floatSensorPins[i], (i < 6) ? INPUT_PULLUP : INPUT);
   }
-
-  for (int i = 0; i < 8; i++) {
-    pinMode(LED_PINS[i], OUTPUT);
+  for (int i = 0; i < numberOfAtomizers; i++) {
+    pinMode(atomizerPins[i], OUTPUT);
+    // Configure LEDC channel for PWM
     ledcSetup(i, PWM_FREQUENCY, PWM_RESOLUTION);
-    ledcAttachPin(LED_PINS[i], i);
+    ledcAttachPin(atomizerPins[i], i);
   }
 }
-
 void loop() {
-  Blynk.run();
-  Blynk.syncVirtual(V1);
+   Blynk.run();
+   Blynk.syncVirtual(V1);
   Blynk.syncVirtual(V2);
   Blynk.syncVirtual(V3);
-  Blynk.syncVirtual(ON_TIME_SLIDER_VPIN);
-  Blynk.syncVirtual(OFF_TIME_SLIDER_VPIN);
+  Blynk.syncVirtual(vPinAtomizerOnTime);
+  Blynk.syncVirtual(vPinAtomizerOffTime);
   Blynk.syncVirtual(TEMP_SWITCH_VPIN);
   Blynk.syncVirtual(V23);
   Blynk.syncVirtual(V24);
-
   unsigned long currentTime = millis();
 
   if (isOnTime) {
+    // Check if it's time to turn off the atomizers
     if (currentTime - prevTime >= atomizerOnTime * 1000) {
       isOnTime = !isOnTime;
       prevTime = currentTime;
     }
   } else {
+    // Check if it's time to turn on the atomizers
     if (currentTime - prevTime >= atomizerOffTime * 1000) {
       isOnTime = !isOnTime;
       prevTime = currentTime;
     }
   }
 
-  for (int i = 0; i < 8; i++) {
-    if (ledState[i] && isOnTime) {
-      ledcWrite(i, PWM_MAX_DUTY_CYCLE);
+  // Control atomizer LEDs based on the current state
+  for (int i = 0; i < numberOfAtomizers; i++) {
+    if (isOnTime) {
+      if (atomizerState[i]) {
+        ledcWrite(i, PWM_MAX_DUTY_CYCLE);
+      } else {
+        ledcWrite(i, 0);
+      }
     } else {
-      ledcWrite(i, 0);
+      ledcWrite(i, 0);  // Turn off all LEDs during off-time
     }
   }
-   float esp32Temperature = (temperatureRead() - 32) / 1.8; // Convert to Celsius
+
+  // You can include additional code here as needed
+
+  delay(2000);
+    float esp32Temperature = (temperatureRead() - 32) / 1.8; // Convert to Celsius
    Blynk.virtualWrite(V25, esp32Temperature);
 Serial.print("ESP32 Temperature: ");
   Serial.print(esp32Temperature);
@@ -82,20 +93,20 @@ Serial.print("ESP32 Temperature: ");
   Serial.print(esp32Temperature);
   Serial.println("°C");
 
-  }
-
-  delay(2000);
-}
-
+  // You can inject your own code or combine it with other sketches.
+  // Check other examples on how to communicate with Blynk. Remember
+  // to avoid delay() function!
+}}
 void readFloatData() {
-  for (int i = 0; i < NUM_FLOAT_SWITCHES; i++) {
-    floatSensorStates[i] = digitalRead(FLOAT_SENSOR_PINS[i]);
-    Blynk.virtualWrite(FLOAT_SENSOR_VPINS[i], floatSensorStates[i]);
+  for (int i = 0; i < numberOfFloatSensors; i++) {
+    floatSensorStates[i] = digitalRead(floatSensorPins[i]);
+    Blynk.virtualWrite(vPinFloatSwitches[i], floatSensorStates[i]);
   }
 }
-
+// Attach Virtual Pins
 BLYNK_WRITE_DEFAULT() {
   int pin = request.pin;
+  // Check if it's handling the correct pin for the temperature switch
   if (pin == TEMP_SWITCH_VPIN) {
     // Handle the switch state
     int switchState = param.asInt();
@@ -109,15 +120,14 @@ BLYNK_WRITE_DEFAULT() {
       Serial.print(humidity);
       Serial.println("%");
     }
-  } else if (pin >= V1 && pin <= V8) {
-    ledState[pin - V1] = (param.asInt() == 1);
+  } else if (pin >= V1 && pin <= V10) {
+    atomizerState[pin - V1] = (param.asInt() == 1);
   }
 }
 
 BLYNK_WRITE(V4) {
   atomizerOnTime = param.asInt();
 }
-
 BLYNK_WRITE(V5) {
   atomizerOffTime = param.asInt();
 }
